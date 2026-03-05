852e55b030fa739e48f2119313ab0eae9ec3641f
Nihal Patidar <nihalpatidar14@gmail.com>
2026-03-06 01:39:22 +0530

    Outline Change on Focus and Worked on Functionality of Drop down toggle in Search Bar

---
+++ searchBar.js
@@ -0,0 +1,38 @@
+
+// Change the Outline Color on Focus the Input
+// Using Immediate Invoke Function to avoid Immediate calling manually
+(function searchAreaChangeOutline(){
+
+    // On focus
+    document.getElementById('input_city_name').addEventListener('focus',()=>{
+        document.getElementById('search_area').classList.remove('outline-cyan-500');
+        document.getElementById('search_area').classList.add('outline-blue-500');
+    })
+    
+    // On Focus Out
+    document.getElementById('input_city_name').addEventListener('blur',()=>{
+        document.getElementById('search_area').classList.remove('outline-blue-500');
+        document.getElementById('search_area').classList.add('outline-cyan-500');
+    })
+
+})();
+// searchAreaChangeOutline();
+
+
+// Adding functionalities to show and hide Recent Search DropDown.
+
+let drop_down_bar_open = false ;
+(function dropDownBarToggle(){
+    document.getElementById('drop_down_bar').addEventListener('click',()=>{
+        if(drop_down_bar_open){ // open 
+            document.getElementById('recent_city_dropdown').classList.add('hidden');
+            drop_down_bar_open = !drop_down_bar_open ;
+        }else{ // close 
+            document.getElementById('recent_city_dropdown').classList.remove('hidden');
+            drop_down_bar_open = !drop_down_bar_open ;
+
+        }
+    })
+
+
+})();
